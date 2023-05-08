70f1dd3ba3b9cf002dc9a6054a352621caf871b8
Mohammad_Es <mohammad.es0281@gmail.com>
2023-05-08 17:32:47 +0430

    add social media button (without icon)

---
+++ lib/layers/presentation/auth/signup/widgets/social_media_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaButton extends StatelessWidget {
+  final void Function() onPressed;
+  final String text;
+
+  const SocialMediaButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final double containerWidth = screenWidth;
+
+    return SizedBox(
+      width: containerWidth,
+      height: 56,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(
+            Theme.of(context).colorScheme.background,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+              side: BorderSide(
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 12),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
