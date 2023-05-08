2f441730d0e5590846cc23819de7ee29a3bef8fd
Mohammad_Es <mohammad.es0281@gmail.com>
2023-05-08 16:03:58 +0430

    add app primary button

---
+++ lib/layers/presentation/shared/widgets/app_primary_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppPrimaryButton extends StatelessWidget {
+  final void Function() onPressed;
+  final String text;
+
+  const AppPrimaryButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final double containerWidth = screenWidth;
+
+    return SizedBox(
+      width: containerWidth,
+      height: 56,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+          ),
+        ),
+        child: Text(
+          text,
+          style: Theme.of(context).textTheme.labelLarge?.copyWith(
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+      ),
+    );
+  }
+}
