a8c71c056469b5f39c819e7275b9953a459cea6f
sorour rokni <soroorrokni@gmail.com>
2023-05-09 23:14:31 +0430

    fix size of recent whatched items

--- lib/layers/presentation/home/screens/home_screen.dart
+++ lib/layers/presentation/home/screens/home_screen.dart
@@ -9,7 +9,6 @@ import 'package:supermedia/layers/presentation/home/bloc/trend_items/trend_items
 import 'package:supermedia/layers/presentation/home/bloc/trend_items/trend_items_event.dart';
 import 'package:supermedia/layers/presentation/home/widgets/category_list.dart';
 import 'package:supermedia/layers/presentation/home/widgets/data.dart';
-import 'package:supermedia/layers/presentation/home/widgets/post_item.dart';
 import 'package:supermedia/layers/presentation/home/widgets/post_list.dart';
 import 'package:supermedia/layers/presentation/home/widgets/story_list.dart';
 
@@ -47,147 +46,141 @@ class _HomeFormState extends State<_HomeForm> {
 
   @override
   Widget build(BuildContext context) {
-
     context.read<RecentItemsBloc>().add(LoadRecentItems());
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 55, 15, 0),
-              child: Image.asset(
-                'assets/images/icons/profile_icon.png',
-                width: 40,
-                height: 40,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 52, 0, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'Hi, Mohammad',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  Text(
-                    'Explore today items',
-                    style: TextStyle(
-                        fontSize: 12,
-                        color: Color.fromARGB(255, 156, 164, 171)),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 78, 8, 0),
-              child: CustomSlidingSegmentedControl(
-                children: {
-                  0: Text(
-                    'All',
-                    style:
-                        _selectedSegment == DateFilter.all ? selected : defaul,
-                  ),
-                  1: Text('Today',
-                      style: _selectedSegment == DateFilter.today
-                          ? selected
-                          : defaul)
-                },
-                fixedWidth: 53,
-                height: 24,
-                onValueChanged: (value) {
-                  setState(() {
-                    if (value == 0) {
-                      _selectedSegment = DateFilter.all;
-                      context
-                          .read<TrendItemsBloc>()
-                          .add(LoadTrendItems(dateFilter: DateFilter.all));
-                    } else {
-                      _selectedSegment = DateFilter.today;
-                      context
-                          .read<TrendItemsBloc>()
-                          .add(LoadTrendItems(dateFilter: DateFilter.today));
-                    }
-                  });
-                },
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 236, 241, 246),
-                    borderRadius: BorderRadius.circular(24)),
-                thumbDecoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 81, 78, 182),
-                  borderRadius: BorderRadius.circular(24),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 55, 15, 0),
+                child: Image.asset(
+                  'assets/images/icons/profile_icon.png',
+                  width: 40,
+                  height: 40,
                 ),
-                innerPadding: const EdgeInsets.all(2),
               ),
-            )
-          ],
-        ),
-        StoryList(
-          stories: stories,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'See what’s good for you!',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff0D253C)),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 52, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Hi, Mohammad',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      'Explore today items',
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Color.fromARGB(255, 156, 164, 171)),
+                    )
+                  ],
+                ),
               ),
+              Expanded(
+                child: Container(),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 78, 8, 0),
+                child: CustomSlidingSegmentedControl(
+                  children: {
+                    0: Text(
+                      'All',
+                      style: _selectedSegment == DateFilter.all
+                          ? selected
+                          : defaul,
+                    ),
+                    1: Text('Today',
+                        style: _selectedSegment == DateFilter.today
+                            ? selected
+                            : defaul)
+                  },
+                  fixedWidth: 53,
+                  height: 24,
+                  onValueChanged: (value) {
+                    setState(() {
+                      if (value == 0) {
+                        _selectedSegment = DateFilter.all;
+                        context
+                            .read<TrendItemsBloc>()
+                            .add(LoadTrendItems(dateFilter: DateFilter.all));
+                      } else {
+                        _selectedSegment = DateFilter.today;
+                        context
+                            .read<TrendItemsBloc>()
+                            .add(LoadTrendItems(dateFilter: DateFilter.today));
+                      }
+                    });
+                  },
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 236, 241, 246),
+                      borderRadius: BorderRadius.circular(24)),
+                  thumbDecoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 81, 78, 182),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  innerPadding: const EdgeInsets.all(2),
+                ),
+              )
             ],
           ),
-        ),
-        const CategoryList(),
-        const Padding(
-          padding: EdgeInsets.fromLTRB(24, 22, 0, 0),
-          child: Text(
-            'Recent watched',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          StoryList(
+            stories: stories,
           ),
-        ),
-        BlocBuilder<RecentItemsBloc, RecentItemsState>(
-          builder: (context, state) {
-            if (state is RecentItemsInitial) {
-              // todo center vertically
-              return Container(
-                color: Colors.red,
-                child: const Center(
-                  child: Text('Empty List.'),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'See what’s good for you!',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff0D253C)),
                 ),
-              );
-            } else if (state is RecentItemsLoading) {
-              return const Expanded(
-                  child: Center(
-                child: CircularProgressIndicator(),
-              ));
-            } else if (state is RecentItemsSuccess) {
-              var result = state.result;
-
-              return PostList(
-                items: List.generate(
-                  result.length,
-                  (index) {
-                    var mediaMetadata = result[index];
-                    return PostItem(
-                      mediaMetadata: mediaMetadata,
-                    );
-                  },
-                ),
-              );
-            } else {
-              return Container();
-            }
-          },
-        ),
-      ],
+              ],
+            ),
+          ),
+          const CategoryList(),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(24, 22, 0, 16),
+            child: Text(
+              'Recent watched',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ),
+          BlocBuilder<RecentItemsBloc, RecentItemsState>(
+            builder: (context, state) {
+              if (state is RecentItemsInitial) {
+                // todo center vertically
+                return Container(
+                  color: Colors.red,
+                  child: const Center(
+                    child: Text('Empty List.'),
+                  ),
+                );
+              } else if (state is RecentItemsLoading) {
+                return const Expanded(
+                    child: Center(
+                  child: CircularProgressIndicator(),
+                ));
+              } else if (state is RecentItemsSuccess) {
+                var result = state.result;
+                return PostList(
+                  items: result,
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/layers/presentation/home/widgets/post_list.dart
+++ lib/layers/presentation/home/widgets/post_list.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:supermedia/layers/domain/entities/media_metadata.dart';
 import 'package:supermedia/layers/presentation/home/widgets/post_item.dart';
 
 class PostList extends StatelessWidget {
-  final List<PostItem> items;
+  final List<MediaMetadata> items;
 
   const PostList({super.key, required this.items});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: ListView(children: items));
+    return ListView.builder(
+      shrinkWrap: true,
+      itemExtent: 140,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = items[index];
+        return PostItem(
+          mediaMetadata: item,
+        );
+      },
+    );
   }
 }
